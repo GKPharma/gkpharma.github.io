27082f7ad99c08bc48cb49ec687854d4f23673e7
nguyenntt3 <nguyenntt3@vng.com.vn>
2021-09-16 10:52:52 +0700

    update ticketbox

--- web-core/src/App.js
+++ web-core/src/App.js
@@ -4,16 +4,41 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import Checkout from './checkout/Checkout.js';
+import Checkout from './ticket/Checkout.js';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#3f51b5',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#ff4081',
+      main: '#f50057',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#fff',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://gkpharma.github.io/">
+        GKPharma
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -21,15 +46,12 @@ function Copyright() {
   );
 }
 
-const theme = createTheme();
-
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar
         position="absolute"
-        color="default"
         elevation={0}
         sx={{
           position: 'relative',
@@ -37,11 +59,14 @@ export default function App() {
         }}>
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            Gia Khanh Pharmacy
+            GKPharma
           </Typography>
         </Toolbar>
       </AppBar>
-      <Checkout />
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Checkout />
+        <Copyright />
+      </Container>
     </ThemeProvider>
   );
 }
--- web-core/src/ticket/Checkout.js
+++ web-core/src/ticket/Checkout.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -8,23 +7,9 @@ import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
@@ -53,7 +38,6 @@ export default function Checkout() {
   };
 
   return (
-    <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
             Checkout
@@ -99,7 +83,5 @@ export default function Checkout() {
             )}
           </React.Fragment>
         </Paper>
-        <Copyright />
-      </Container>
   );
 }
